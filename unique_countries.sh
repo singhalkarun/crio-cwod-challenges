ab791f1bfd59001f22b99dced9face917d0ec900
Karun Agarwal <agarwalkarun01@gmail.com>
2021-01-13 15:12:04 +0530

    Update unique_countries.sh

--- unique_countries.sh
+++ unique_countries.sh
@@ -1,22 +1,22 @@
-#!/bin/bash  
+#!/bin/bash
 
-grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" /var/log/qeats-server.log | sort | uniq > ~/uniqueIp.txt  
+grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" /var/log/qeats-server.log | sort | uniq > uniqueIp.txt
 
-touch countries.txt  
+if test -f countries.txt; then
+  rm countries.txt
+fi
 
-while IFS= read -r line; do     
-  url="https://tools.keycdn.com/geo.json?host=$line"     
-  curl --silent -o ~/cdnapi.txt $url     
-  grep -oE "\"country_name\"\:\"[A-za-z]{1,}\"" ~/cdnapi.txt >> countries.txt 
-done < uniqueIp.txt  
+regex="\"country_name\"\:\"([A-Za-z]{1,})\""
+while IFS= read -r line; do
+  url="https://tools.keycdn.com/geo.json?host=$line"
+  curl --silent -o cdnapi.txt $url
+  country="$(grep -oE "\"country_name\"\:\"[A-za-z]{1,}\"" cdnapi.txt)"
 
-sort countries.txt | uniq > uniqueCountries.txt  
+  if [[ $country =~ $regex ]]; then 
+    countryFormatted="${BASH_REMATCH[1]}"
+    echo $countryFormatted >> countries.txt
+  fi
+done < uniqueIp.txt
 
-regex="\"country_name\"\:\"([A-Za-z]{1,})\"" 
-
-while IFS= read -r line;do 
-  if [[ $line =~ $regex ]] 
-    then country="${BASH_REMATCH[1]}" 
-    echo $country 
-  fi 
-done < uniqueCountries.txt
+sort countries.txt | uniq > uniqueCountries.txt
+cat uniqueCountries.txt
